1b46c7a132744143736f0ecd7f124e535f454be3
HW-XMTeam <denysaputrayoman@gmail.com>
2022-10-07 20:08:41 +0700

    hasdi-v

---
+++ src/menunya.js
@@ -0,0 +1,25 @@
+const menunya = `
+  - ALL MENU -
+•  BUGMENU 
+•  FUNMENU 
+•  MAINMENU 
+•  VOICEMENU 
+•  GROUPMENU 
+•  OWNERMENU 
+•  ISLAMICMENU 
+•  SEARCHMENU 
+•  EPHOTOMENU 
+•  RANDOMMENU 
+•  TEKSPROMENU 
+•  PRIMBONMENU 
+•  CONVERTMENU 
+•  WEBZONEMENU
+•  DATABASEMENU 
+•  PHOTOOXYMENU
+•  DOWNLOADMENU
+•  ANONYMOUSMENU
+
+Hasdi Makers 👿
+
+`
+exports.menunya = menunya
